ee262e6260ac93f5a3c18a8cf802e1435bd8697b
Szymon K <155658612+SzymonK16@users.noreply.github.com>
2025-10-05 11:58:52 +0200

    Solve assigment 1

---
+++ Assigment_1/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+void main() {
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Podaj swoję imie:");
+    String yourName = scn.nextLine();
+    System.out.println("Witaj " +yourName);
+}
